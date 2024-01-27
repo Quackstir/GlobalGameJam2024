1059c21cb1e11cc5a412c73bf47da5b704bbaf93
Soulfairs <79177447+Soulfairs@users.noreply.github.com>
2024-01-27 14:39:52 -0800

    Main Menu Baseline

---
+++ GGJ 2024 Game/main_menu.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+func _on_credits_button_pressed():
+	get_tree().change_scene_to_file("res://Credits.tscn")
+
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://Mock Game.tscn")
+
+func _on_settings_button_pressed():
+	get_tree().change_scene_to_file("res://Settings.tscn")
+
+func _on_quit_button_pressed():
+	get_tree().change_scene_to_file("res://Quit Confirm.tscn")
