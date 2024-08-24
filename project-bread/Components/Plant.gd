e79b4c2b2ef9a81d3eadd40e2b4ff6fa73780f94
Quackstir <51137567+Quackstir@users.noreply.github.com>
2024-08-24 13:32:43 -0700

    polishing up the plants

--- project-bread/Components/Plant.gd
+++ project-bread/Components/Plant.gd
@@ -3,22 +3,44 @@ class_name Plant extends Node2D
 @onready var timer: Timer = $Timer
 @onready var sprite_2d: Sprite2D = $Sprite2D
 
-@export var maxStages:int
-var currentStage:int
+var EndStageReward:int
+var currentStage = 0
+var readyToGrow = false
+
+@export var Stages:Array[r_PlantStage]
+@onready var rich_text_label: RichTextLabel = $RichTextLabel
+
 
 var isWatered:bool = false:
 	set(newValue):
-		isWatered = newValue
-		if isWatered:
-			sprite_2d.self_modulate = Color.TEAL
-			_begin_stage()
+		if !isWatered and newValue:
+			#sprite_2d.self_modulate = Color.TEAL
+			rich_text_label.text = "Growing"
+			if readyToGrow:
+				_begin_stage()
 			timer.start()
+		isWatered = newValue
+
+func _ready() -> void:
+	setStage()
+	
+func setStage():
+	sprite_2d.texture = Stages[currentStage].Sprite
+	timer.wait_time = Stages[currentStage].TimeToGrow
+	EndStageReward = Stages[currentStage].RewardMoney
 
 func _begin_stage():
-	#GameManager.money
-	GameManagerScript.money += 10
-	#GameManager.tools
+	GameManagerScript.money += EndStageReward
+	currentStage += 1
+	readyToGrow = false
+	if currentStage < Stages.size():
+		rich_text_label.text = "Water Me"
+		setStage()
+	else:
+		queue_free()
 
 func _on_timer_timeout() -> void:
-	sprite_2d.self_modulate = Color.GREEN
+	#sprite_2d.self_modulate = Color.GREEN
+	#rich_text_label.text = "Harvest me"
 	isWatered = false
+	readyToGrow = true
